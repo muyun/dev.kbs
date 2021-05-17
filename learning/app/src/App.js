4d1ab5523cc4280cafaba0111955d4f89b0c7132
muyun <wenlzhao@gmail.com>
2021-05-17 17:46:28 +0800

    react learning update

--- learning/app/src/App.js
+++ learning/app/src/App.js
@@ -4,9 +4,31 @@ import Table from './Table'
 
 class App extends Component {
     render() {
+
+        // data
+        const characters = [
+            {
+              name: 'Charlie',
+              job:  'Janitor',
+            },
+            {
+              name: 'Mac',
+              job:  'Bouncer',
+            },
+            {
+              name: 'Dee',
+              job:  'Aspiring actress',
+            },
+            {
+              name: 'Dennis',
+              job:  'Bartender',
+            },
+          ]
+
         return (
+            // pass the data through {characters} to the child (Table) componene 
             <div className="container">
-              <Table />
+              <Table characterData={characters} />
             </div>
         )
     }
